3b67339c89214bd6881e499c6a016d2abb5c8821
AndrewAche <121398221+AndrewAche@users.noreply.github.com>
2023-11-26 19:41:38 +0300

    Update variables.tf

--- Управляющие конструкции в коде Terraform./variables.tf
+++ Управляющие конструкции в коде Terraform./variables.tf
@@ -69,3 +69,20 @@ variable "vms_settings"{
     frac=100
   }]
 }
+
+###yandex_compute_instance vars
+
+variable "vm_web_cores" {
+  type        = number
+  default     = 2
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = 1
+}
+
+variable "vm_web_fraction" {
+  type        = number
+  default     = 5
+}
