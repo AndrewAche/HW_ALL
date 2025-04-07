01fd6b246c1759cc0ce716632ad8cc2c0e1b2df7
AndrewAche <121398221+AndrewAche@users.noreply.github.com>
2025-04-08 00:21:54 +0300

    Update variables.tf

--- Диплом/terraform/bucket/variables.tf
+++ Диплом/terraform/bucket/variables.tf
@@ -1,53 +1,29 @@
-  GNU nano 5.4                                                                                                                                bucket.tf                                                                                                                                         // Create SA
-resource "yandex_iam_service_account" "sa-terraform" {
-    name      = "sa-terraform"
+variable "yandex_cloud_id" {
+  default = "b1gpm433o1t8c6iqf889"
 }
 
-// Grant permissions
-resource "yandex_resourcemanager_folder_iam_member" "terraform-editor" {
-    folder_id = var.yandex_folder_id
-    role      = "editor"
-    member    = "serviceAccount:${yandex_iam_service_account.sa-terraform.id}"
-    depends_on = [yandex_iam_service_account.sa-terraform]
+variable "yandex_folder_id" {
+  default = "b1g28u34etbvshh9gi63"
 }
 
-// Create Static Access Keys
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-    service_account_id = yandex_iam_service_account.sa-terraform.id
-    description        = "static access key"
+variable "a-zone" {
+  default = "ru-central1-a"
 }
 
-// Use keys to create bucket
-resource "yandex_storage_bucket" "netology-bucket" {
-    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-    bucket = "acheusov-netology-bucket"
-    acl    = "private"
-    force_destroy = true
+variable "subnet-zones" {
+  type    = list(string)
+  default = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
 }
 
-// Create "local_file" for "backendConf"
-resource "local_file" "backendConf" {
-  content  = <<EOT
-endpoint = "storage.yandexcloud.net"
-bucket = "${yandex_storage_bucket.netology-bucket.bucket}"
-region = "ru-central1"
-key = "terraform/terraform.tfstate"
-access_key = "${yandex_iam_service_account_static_access_key.sa-static-key.access_key}"
-secret_key = "${yandex_iam_service_account_static_access_key.sa-static-key.secret_key}"
-skip_region_validation = true
-skip_credentials_validation = true
-EOT
-  filename = "../backend.key"
+variable "ubuntu-2004-lts" {
+  default = "fd85u0rct32prepgjlv0"
 }
 
-// Add "backendConf" to bucket
-resource "yandex_storage_object" "object-1" {
-    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-    bucket = yandex_storage_bucket.netology-bucket.bucket
-    key = "terraform.tfstate"
-    source = "./terraform.tfstate"
-    acl    = "private"
-    depends_on = [yandex_storage_bucket.netology-bucket]
+variable "cidr" {
+  type    = map(list(string))
+  default = {
+    stage = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  }
 }
+
+
